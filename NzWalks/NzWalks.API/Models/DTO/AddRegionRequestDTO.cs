e0fcd828523b333d52540ed51405d40425ef8af2
Phulsing <phulsing@deccansoft.net>
2026-02-08 15:23:16 +0530

    Add region creation functionality to RegionsController
    Introduces a new `Create` action method in the `RegionsController`
    to handle the creation of regions using an `AddRegionRequestDTO`.
    This method maps the DTO to a domain model, saves it to the
    database, and returns a `RegionDTO` with the created region's
    details. A new `AddRegionRequestDTO` class is also added to
    define the necessary properties for region creation.

---
+++ NzWalks/NzWalks.API/Models/DTO/AddRegionRequestDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace NzWalks.API.Models.DTO
+{
+    public class AddRegionRequestDTO
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string? RegionImageUrl { get; set; } = null;
+    }
+}

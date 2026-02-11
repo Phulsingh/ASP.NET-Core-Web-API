85868f53cdd82e4a48769dd7ccf89f7d8feb5f2b
Phulsing <phulsing@deccansoft.net>
2026-02-11 23:30:16 +0530

    Refactor RegionsController to use repository pattern
    This commit refactors the `RegionsController` to utilize a repository pattern for data access, replacing direct interactions with `dbContext` with calls to `regionRepository` methods like `GetAllAsync`, `GetByIdAsync`, `CreateAsync`, and `UpdateAsync`.
    
    New method signatures are added to the `IRegionRepository` interface, and the `SQLRegionRepository` class is updated to implement these methods, ensuring proper data access for retrieving, creating, and updating regions.
    
    These changes improve the separation of concerns, maintainability, and testability of the codebase.

--- NzWalks/NzWalks.API/Repositories/SQLRegionRepository.cs
+++ NzWalks/NzWalks.API/Repositories/SQLRegionRepository.cs
@@ -12,9 +12,37 @@ namespace NzWalks.API.Repositories
         {
             this.dbContext = dbContext;
         }
+
         public async Task<List<Region>> GetAllAsync()
         {
             return await dbContext.Regions.ToListAsync();
+        }
+
+        public async Task<Region?> GetByIdAsync(Guid id)
+        {
+            return await dbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+        }
+        public async Task<Region> CreateAsync(Region region)
+        {
+            await dbContext.Regions.AddAsync(region);
+            await dbContext.SaveChangesAsync();
+            return region;
+        }
+
+        public async Task<Region> UpdateAsync(Guid id, Region region)
+        {
+            var existingRegion = await dbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+            if(existingRegion == null)
+            {
+                return null;
+            }
+
+            existingRegion.Name = region.Name;
+            existingRegion.Code = region.Code;
+            existingRegion.RegionImageUrl= region.RegionImageUrl;
+
+            await dbContext.SaveChangesAsync();
+            return existingRegion;
         }
     }
 }

d6709941943bc06d9165be4b45619a24920e544c
Phulsing <phulsing@deccansoft.net>
2026-02-13 23:04:53 +0530

    Integrate AutoMapper into RegionsController
    This commit introduces AutoMapper to streamline the mapping
    between domain models and DTOs in the RegionsController.
    The constructor is updated to accept an IMapper instance,
    and manual mapping is replaced with AutoMapper's functionality.
    A new AutoMapperProfile class is created to define the
    necessary mappings. The project file is updated to include
    AutoMapper as a dependency, and the Program.cs file is
    modified to register AutoMapper with the dependency injection
    container.

---
+++ NzWalks/NzWalks.API/Automapper/AutoMapperProfile.cs
@@ -0,0 +1,17 @@
+﻿
+using AutoMapper;
+using NzWalks.API.Models.Domain;
+using NzWalks.API.Models.DTO;
+
+namespace NzWalks.API.Automapper
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Region, RegionDTO>().ReverseMap();
+            CreateMap<AddRegionRequestDTO, Region>().ReverseMap();
+            CreateMap<UpdateRegionRequestDTO, Region>().ReverseMap();
+        }
+    }
+}

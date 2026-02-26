b7696dfb08336f5ac3ee8e0d486a6941485a855d
Phulsing <phulsing@deccansoft.net>
2026-02-26 23:07:19 +0530

    Add authentication support with NZWalksAuthDbContext
    Introduce NZWalksAuthDbContext for user authentication and role management using ASP.NET Core Identity. Update connection strings in appsettings.json and configure the context in Program.cs. Create necessary tables and seed roles "Reader" and "Writer" through migrations. Include model snapshot and designer files for schema tracking.

---
+++ NzWalks/NzWalks.API/Data/NZWalksAuthDbContext.cs
@@ -0,0 +1,44 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NzWalks.API.Data
+{
+    public class NZWalksAuthDbContext : IdentityDbContext 
+
+    {
+        public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options) : base (options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "0f1b6829-bcc4-4d81-90ee-3283d769aa1b";
+            var writerRoleId = "b8be6f6f-bb9c-4fca-bea5-5003c77e9f44";
+
+            var Role = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "READER".ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id = writerRoleId, 
+                    ConcurrencyStamp = writerRoleId,
+                    Name = "Writer",
+                    NormalizedName = "WRITER".ToUpper()
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(Role);
+        }
+    }
+}
---
+++ NzWalks/NzWalks.API/Migrations/NZWalksAuthDb/20260226173101_SeedRoles .cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace NzWalks.API.Migrations.NZWalksAuthDb
+{
+    /// <inheritdoc />
+    public partial class SeedRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "0f1b6829-bcc4-4d81-90ee-3283d769aa1b", "0f1b6829-bcc4-4d81-90ee-3283d769aa1b", "Reader", "READER" },
+                    { "b8be6f6f-bb9c-4fca-bea5-5003c77e9f44", "b8be6f6f-bb9c-4fca-bea5-5003c77e9f44", "Writer", "WRITER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "0f1b6829-bcc4-4d81-90ee-3283d769aa1b");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b8be6f6f-bb9c-4fca-bea5-5003c77e9f44");
+        }
+    }
+}

6c550ec08caddb7f0698e3be01aa29523292306e
Phulsing <phulsing@deccansoft.net>
2026-02-28 13:02:12 +0530

    Add ASP.NET Core Identity for user authentication
    Implement user registration with roles and validation.
    Configure Identity services in Program.cs, create AuthController
    for handling registration, and define RegisterRequestDTO for
    data transfer.

---
+++ NzWalks/NzWalks.API/Controllers/AuthController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NzWalks.API.Models.DTO;
+using System.Runtime.CompilerServices;
+
+namespace NzWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+         private readonly UserManager<IdentityUser> userManager;
+
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        //Post : api/Auth/Register
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDTO registerRequestDTO)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = registerRequestDTO.Username,
+                Email = registerRequestDTO.Username,
+            };
+
+            var identityResult = await userManager.CreateAsync(identityUser, registerRequestDTO.Password);
+
+            if (identityResult.Succeeded)
+            {
+                //Add Roles to this User
+                if (registerRequestDTO.Roles != null && registerRequestDTO.Roles.Any())
+                {
+                    identityResult = await userManager.AddToRolesAsync(identityUser, registerRequestDTO.Roles);
+
+                    if (identityResult.Succeeded)
+                    {
+                        return Ok("User Registered Successfully");
+                    }
+                }
+     
+            }
+
+            return BadRequest("User Registration Failed");
+        }
+
+    }
+}

2cc83e08d62ed6c163bc8b963573471ee9fc82ee
Phulsing <phulsing@deccansoft.net>
2026-02-28 23:10:13 +0530

    Add Images entity and upload functionality
    Introduce a new `Images` entity to the database context with a corresponding `DbSet<Images>` in `NZWalksDbContext.cs`. Create migration files to establish the `Images` table and update the `LengthInKm` property in the `Walks` table from `int` to `double`.
    
    Add an `ImagesController` for handling image uploads, including validation for file type and size. Define the `Images` class to represent the entity and create a new DTO, `ImageUploadRequestDTO`, for transferring image upload data. Update the `OnModelCreating` method and the `DbContextModelSnapshot` to reflect the new schema.

---
+++ NzWalks/NzWalks.API/Controllers/ImagesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NzWalks.API.Models.DTO;
+
+namespace NzWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        [HttpPost]
+        [Route("upload")]
+        public async Task<IActionResult> UploadImage([FromForm] ImageUploadRequestDTO request)
+
+        {
+            ValidationFileUpload(request);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok("File Uploaded Successfully");
+        }
+
+        private void ValidationFileUpload(ImageUploadRequestDTO request)
+        {
+            var allowedExtension = new string[] { ".jpg", ".jpeg", ".png", ".gif" };
+
+            if (!allowedExtension.Contains(Path.GetExtension(request.File.FileName)))
+            {
+                ModelState.AddModelError("File", "UnSupported File Extension");
+            }
+            if (request.File.Length > 10485760)
+            {
+                ModelState.AddModelError("File", "File Size Exceeds 10MB");
+
+            }
+        }
+    }
+}
---
+++ NzWalks/NzWalks.API/Models/DTO/ImageUploadRequestDTO.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Routing.Constraints;
+using System.ComponentModel.DataAnnotations;
+
+namespace NzWalks.API.Models.DTO
+{
+    public class ImageUploadRequestDTO
+    {
+        [Required]
+        public IFormFile File { get; set; }
+
+        [Required]
+        public string FileName { get; set; }
+        public string? FileDescription { get; set; }
+    }
+}
